85494680499b5135a105f6898939a8875529143a
Ahmed Hamdi <ahamdi@appswave.io>
2025-01-22 14:26:45 +0200

    change color of btn

--- src/components/common/ContactFormFields.tsx
+++ src/components/common/ContactFormFields.tsx
@@ -94,7 +94,7 @@ export default function ContactFormFields() {
       <button
         type="submit"
         disabled={isSubmitting}
-        className="w-full py-4 bg-[#F1911B] text-white rounded-lg hover:bg-[#e08616] transition-all duration-300 disabled:opacity-50 disabled:cursor-not-allowed"
+        className="w-full py-4 bg-[var(--main-color1)] text-white rounded-lg hover:bg-[#e08616] transition-all duration-300 disabled:opacity-50 disabled:cursor-not-allowed"
       >
         {isSubmitting ? 'Submitting...' : t('form.submit')}
       </button>
