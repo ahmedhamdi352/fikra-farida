cf830c6a94232fb0de03c21fdf0ebee2071b9cb7
Ahmed Hamdi <ahamdi@appswave.io>
2025-01-27 16:30:51 +0200

    update hero

--- src/components/views/sections/HeroSection.tsx
+++ src/components/views/sections/HeroSection.tsx
@@ -1,12 +1,11 @@
 import Image from 'next/image'
 // import { useTranslations } from 'next-intl'
 import homeBg from 'assets/images/homePageBackground.png'
-
 export default function HeroSection() {
   // const t = useTranslations('home')
 
   return (
-    <section className="animate-fadeIn relative min-h-fit lg:min-h-screen flex items-center">
+    <section className="animate-fadeIn relative min-h-fit lg:min-h-[90vh] flex items-center">
       {/* Background Image */}
       <div className="absolute inset-0 -z-10">
         <Image
@@ -18,35 +17,66 @@ export default function HeroSection() {
         />
       </div>
 
-      <div className="container mx-auto px-4 py-20 text-center">
-        <div className="max-w-4xl mx-auto">
-          <p className="text-yellow-400 text-body mb-4 uppercase tracking-wider">
-            SHARE ANY THING & MORE
-          </p>
-          <h1 className="text-h1 font-bold text-white mb-6">
-            DIGITAL BUSINESS CARD
-          </h1>
-          <h2 className="text-h2 font-bold text-yellow-400 mb-8">
-            THE FUTURE OF BUSINESS COMMUNICATION
+      <div className="container mx-auto px-4 py-20 my-auto text-center">
+        <div className="flex items-center justify-center mb-5 animate-bounce">
+          <h2 className="text-white text-h2 font-semibold uppercase tracking-wider relative inline-block">
+            SHARE ANY THING{" "} <br className='lg:hidden' />
+            <span className="relative text-black font-bold">
+              <span
+                className="absolute  inset-0 -z-10 bg-[url('assets/icons/brush.svg')] bg-no-repeat bg-contain"
+                style={{
+                  height: "50px",
+                  width: "200px",
+                  transform: "translate(-20%, -10px)",
+                }}
+              ></span>
+              & MORE
+            </span>
           </h2>
-          <p className="text-body mb-8 max-w-3xl mx-auto">
-            Communication between people easily and in the fastest way while saving time and money.
-            We can help you easily widen your social network in a new and easy way for all of your clients
-            whether you are an artist, photographer, businessman, salesperson, model, celebrity, recruiter,
-            athlete, business owner, or entrepreneur.
-          </p>
-          <div className="flex flex-wrap justify-center gap-4">
-            <button className="bg-yellow-400 text-black px-8 py-3 rounded-lg font-semibold hover:bg-yellow-500 transition-colors">
-              Products
-            </button>
-            <button className="border border-yellow-400 text-yellow-400 px-8 py-3 rounded-lg font-semibold hover:bg-yellow-400 hover:text-black transition-colors flex items-center gap-2">
-              For teams
-              <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
-              </svg>
-            </button>
-          </div>
         </div>
+
+        <div className="flex flex-col-reverse lg:flex-row items-center justify-between gap-8">
+          <div className='flex flex-col rounded-2xl bg-[#3b3b3b] border border-[#FEC400] shadow-lg p-2 py-8
+          lg:border-none lg:bg-transparent lg:shadow-none
+          '>
+            <h1 className="text-h1 font-bold lg:text-left w-full text-white mb-6 animate-fadeInLeft">
+              DIGITAL BUSINESS CARD
+            </h1>
+            <h2 className="text-h2 font-bold lg:text-left w-full text-yellow-400 mb-8 animate-fadeInRight">
+              THE FUTURE OF BUSINESS COMMUNICATION
+            </h2>
+            <p className="text-body lg:text-left mb-8 max-w-3xl w-full mx-auto animate-scaleIn">
+              Communication between people easily and in the fastest way while saving time and money.
+              We can help you easily widen your social network in a new and easy way for all of your clients
+              whether you are an artist, photographer, businessman, salesperson, model, celebrity, recruiter,
+              athlete, business owner, or entrepreneur.
+            </p>
+
+            <div className="flex flex-wrap justify-center gap-4 animate-rotateIn">
+              <button className="bg-yellow-400 text-black px-8 py-3 rounded-lg font-semibold hover:bg-yellow-500 transition-colors">
+                Products
+              </button>
+              <button className="border border-yellow-400 text-yellow-400 px-8 py-3 rounded-lg font-semibold hover:bg-yellow-400 hover:text-black transition-colors flex items-center gap-2">
+                For teams
+                <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <video
+            src="/c12_site_1.mp4"
+            muted
+            autoPlay
+            loop
+            playsInline
+            width={400}
+            height={400}
+            className="max-h-fit lg:w-[35%] max-w-full rounded-lg shadow-lg bg-transparent"
+          />
+        </div>
+
       </div>
     </section>
   )
--- src/components/views/sections/SmartCardSection.tsx
+++ src/components/views/sections/SmartCardSection.tsx
@@ -10,20 +10,39 @@ export default function SmartCardSection() {
     <section className="relative py-8">
       <div className="container mx-auto px-4">
         <div className="max-w-5xl mx-auto rounded-2xl border border-[#FEC400] p-12 relative overflow-hidden">
-          <div className="text-center mb-12 relative z-10">
-            <p className="text-white/80 uppercase tracking-wider mb-4 text-[20px]">CONNECT WITH A TOUCH</p>
-            <h2 className="text-4xl md:text-5xl font-bold mb-6 text-[32px]">
+          <div className="text-center  relative z-10">
+            {/* <p className="text-white/80 uppercase tracking-wider mb-4 text-[20px]">CONNECT WITH A TOUCH</p> */}
+
+            <div className="flex items-center justify-center mb-5 animate-bounce">
+              <h3 className="text-white text-h3 font-semibold uppercase tracking-wider relative inline-block">
+                CONNECT WITH {" "} <br className='lg:hidden' />
+                <span className="relative text-black font-bold">
+                  <span
+                    className="absolute  inset-0 -z-10 bg-[url('assets/icons/brush.svg')] bg-no-repeat bg-contain"
+                    style={{
+                      height: "50px",
+                      width: "200px",
+                      transform: "translate(-20%, -8px)",
+                    }}
+                  ></span>
+                  A TOUCH
+                </span>
+              </h3>
+            </div>
+
+
+            <h2 className=" font-bold mb-6 text-h3">
               <span className="text-white">Smart & Digital </span>
               <span className="text-[#FEC400]">Business Card</span>
               <span className="text-white"> In Your Hand</span>
             </h2>
-            <p className="text-white/80 text-lg mb-4 text-[20px]">
+            <p className="text-white/80  mb-4 text-body">
               Get digital business cards for yourself or your team,
               <span className='block'>Join millions of professionals</span>
             </p>
 
             {/* Decorative Line */}
-            <div className="flex justify-center my-8">
+            <div className="flex justify-center my-2">
               <svg xmlns="http://www.w3.org/2000/svg" width="464" height="40" viewBox="0 0 464 40" fill="none">
                 <path d="M0 24.1912C47.3156 24.4845 140.596 23.3404 187.974 23.6318C188.038 23.6321 188.097 23.6571 188.143 23.7014L193.968 29.2924C194.08 29.4005 194.263 29.3801 194.349 29.25L212.816 1.32686C212.917 1.17444 213.142 1.17814 213.238 1.33379L227.077 23.8347C227.176 23.9959 227.411 23.9928 227.506 23.8292L230.642 18.4238C230.739 18.2562 230.982 18.2581 231.076 18.4272L242.317 38.5321C242.421 38.7175 242.694 38.6974 242.769 38.4989L255.136 5.94099C255.209 5.74785 255.472 5.722 255.581 5.89712L266.957 24.0738C267.003 24.1468 267.083 24.1912 267.169 24.1912H464" stroke="url(#paint0_linear_862_5901)" strokeWidth="2" />
                 <defs>
@@ -57,28 +76,28 @@ export default function SmartCardSection() {
           </div>
 
           {/* Main Content */}
-          <div className="relative z-10 max-w-2xl">
+          <div className="relative z-10 ">
             {/* Features List */}
             <div className="space-y-8">
-              <div className="flex items-center gap-6">
+              <div className="flex items-center gap-2">
                 <div className="w-16 h-16 relative flex-shrink-0">
                   <Image src={scanningNfc} alt="Device" layout="fill" objectFit="contain" />
                 </div>
-                <p className="text-white text-lg">Works on any type of device.</p>
+                <p className="text-white text-body animate-rotateIn">Works on any type of device.</p>
               </div>
 
-              <div className="flex items-center gap-6">
+              <div className="flex items-center gap-2">
                 <div className="w-16 h-16 relative flex-shrink-0">
                   <Image src={scanningQR} alt="Team" layout="fill" objectFit="contain" />
                 </div>
-                <p className="text-white text-lg">The perfect alternative to the business card for teams and individuals</p>
+                <p className="text-white text-body animate-rotateIn">The perfect alternative to the business card for teams and individuals</p>
               </div>
 
-              <div className="flex items-center gap-6">
+              <div className="flex items-center gap-2">
                 <div className="w-16 h-16 relative flex-shrink-0">
                   <Image src={worldInternet} alt="NFC" layout="fill" objectFit="contain" />
                 </div>
-                <p className="text-white text-lg">Compatible with NFC technology and Qr Code.</p>
+                <p className="text-white text-body animate-rotateIn">Compatible with NFC technology and Qr Code.</p>
               </div>
             </div>
           </div>
